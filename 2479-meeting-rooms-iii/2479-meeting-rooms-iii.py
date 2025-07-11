d5b3a18789d03158a5294b0184934e0df801ff67
Koushik Varma <60594906+koushik-varma-m@users.noreply.github.com>
2025-07-11 14:03:08 -0500

    Time: 219 ms (27.17%), Space: 51.6 MB (14.86%) - LeetHub

---
+++ 2479-meeting-rooms-iii/2479-meeting-rooms-iii.py
@@ -0,0 +1,35 @@
+class Solution:
+    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        meetings.sort()
+        free=[]
+        for i in range(n):
+            heapq.heappush(free,i)
+        print(free)
+        oc=[]
+        d=defaultdict(int)
+        cur=0
+        for s,e in meetings:
+            cur=max(s,cur)
+            while oc and cur>=oc[0][0]:
+                    t,r=heapq.heappop(oc)
+                    heapq.heappush(free,r)
+            if not free:
+                cur,r=heapq.heappop(oc)
+                heapq.heappush(free,r)
+                while oc and cur==oc[0][0]:
+                    t,r=heapq.heappop(oc)
+                    heapq.heappush(free,r)
+            r=heapq.heappop(free)
+            if cur>s:
+                heapq.heappush(oc,[e+cur-s,r])
+            else:
+                heapq.heappush(oc,[e,r])
+                cur=s
+            d[r]+=1
+        ansRoom=0
+        cur=d[0]
+        for i in range(1,n):
+            if d[i]>cur:
+                ansRoom=i
+                cur=d[i]
+        return ansRoom
