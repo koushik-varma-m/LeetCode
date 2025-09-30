50bddf9619f67f3e49781e041d2c8de7ee6fb334
Koushik Varma Mudunuri <60594906+koushik-varma-m@users.noreply.github.com>
2025-09-30 15:03:25 -0400

    Time: 2319 ms (20.49%), Space: 17.5 MB (98.1%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,9 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        def rec(i,j):
+            if i==j:
+                return nums[i]
+            p_l=nums[i]-rec(i+1,j)
+            p_r=nums[j]-rec(i,j-1)
+            return max(p_l,p_r)
+        return rec(0,len(nums)-1)>=0
