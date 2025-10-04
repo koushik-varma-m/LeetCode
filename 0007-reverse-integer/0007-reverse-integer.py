9529cfec2597a20079f3be6c890f3280a9e36ac9
Koushik Varma Mudunuri <60594906+koushik-varma-m@users.noreply.github.com>
2025-10-04 15:00:00 -0400

    Time: 45 ms (18.41%), Space: 17.8 MB (57.21%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = -1 if x<0 else 1
+        x=abs(x)
+        ans=0
+        while(x>0):
+            ans=ans*10+x%10
+            x//=10
+        ans=ans*sign
+        if ans<(-2**31) or ans>=(2**31):
+            return 0
+        return ans
