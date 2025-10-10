29594893d1bcb59a3e43845974e4440c05de7d28
Koushik Varma Mudunuri <60594906+koushik-varma-m@users.noreply.github.com>
2025-10-10 15:41:15 -0400

    Time: 1072 ms (96.57%), Space: 31.5 MB (21.08%) - LeetHub

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.py
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.py
@@ -1,11 +1,10 @@
 class Solution:
     def maximumEnergy(self, energy: List[int], k: int) -> int:
         ans=float(-inf)
-        for i in range(len(energy)):
-            if i+k>=len(energy):
-                if energy[i]>ans:
-                    ans=energy[i]
-                continue
+        for i in range(len(energy)-k):
             if energy[i]>0:
                 energy[i+k]+=energy[i]
+        for i in range(len(energy)-k,len(energy)):
+            if energy[i]>ans:
+                ans=energy[i]
         return ans
