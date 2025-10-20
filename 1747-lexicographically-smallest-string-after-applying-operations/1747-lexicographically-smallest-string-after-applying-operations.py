467d418701f7cd8f59a792549217ea398ab55228
Koushik Varma Mudunuri <60594906+koushik-varma-m@users.noreply.github.com>
2025-10-20 15:28:45 -0400

    Time: 849 ms (22.38%), Space: 38.7 MB (16.08%) - LeetHub

--- 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.py
+++ 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.py
@@ -1,24 +1,21 @@
 class Solution:
     def findLexSmallestString(self, s: str, a: int, b: int) -> str:
-        n = len(s)
-        vis = [False] * n
-        res = s
-        # double the length of s for convenience in extracting the rotated string t
-        s = s + s
-        i = 0
-        while not vis[i]:
-            vis[i] = True
-            for j in range(10):
-                k_limit = 0 if b % 2 == 0 else 9
-                for k in range(k_limit + 1):
-                    # before each accumulation, re-truncate t
-                    t = list(s[i : i + n])
-                    for p in range(1, n, 2):
-                        t[p] = str((int(t[p]) + j * a) % 10)
-                    for p in range(0, n, 2):
-                        t[p] = str((int(t[p]) + k * a) % 10)
-                    t_str = "".join(t)
-                    if t_str < res:
-                        res = t_str
-            i = (i + b) % n
-        return res
+        se = set()
+        def rec(st):
+            if st in se:
+                return 
+            se.add(st)
+            
+            rec(st[-b:]+st[:-b])
+
+            x=""
+            for i,v in enumerate(st):
+                if i%2==0:
+                    x+=v
+                else:
+                    t = int(v)+a
+                    x+=str(t)[-1]
+            rec(x)
+
+        rec(s)
+        return min(se)
