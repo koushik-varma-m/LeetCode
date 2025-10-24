8c7514f6ad15ce1b9806bed773839663dd1db2eb
Koushik Varma Mudunuri <60594906+koushik-varma-m@users.noreply.github.com>
2025-10-24 10:45:04 -0400

    Time: 2918 ms (37.21%), Space: 17.7 MB (88.95%) - LeetHub

--- 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.py
+++ 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.py
@@ -1,14 +1,5 @@
 class Solution:
     def nextBeautifulNumber(self, n: int) -> int:
-        l=len(str(n))
-        if n==0:
-            return 1
-        if n<22:
-            return 22
-        if l<=6:
-            ma=int("1"+l*str(l))
-        else:
-            ma=1224444
         def check(m):
             d=defaultdict(int)
             for i in str(m):
@@ -17,7 +8,6 @@ class Solution:
                 if int(i)!=d[i]:
                     return False
             return True
-        print(ma)
-        for i in range(n+1,ma+1):
+        for i in range(n+1,1224445):
             if check(i):   
                 return i  
