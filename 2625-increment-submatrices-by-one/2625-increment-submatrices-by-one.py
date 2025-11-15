91f4e162540dac8a5159ad23c350d6077b1ae60c
Koushik Varma Mudunuri <60594906+koushik-varma-m@users.noreply.github.com>
2025-11-15 16:42:48 -0500

    Time: 852 ms (15.63%), Space: 39.8 MB (51.16%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.py
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.py
@@ -2,22 +2,15 @@ class Solution:
     def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
         matrix=[[0 for _ in range(n)] for _ in range(n)]
         cur=0
-        while(cur<n):
-            cols=[0 for _ in range(n+1)]
-            for x1,y1,x2,y2 in queries:
-                if x1<=cur<=x2:
-                    cols[y1]+=1
-                    cols[y2+1]-=1
+        for x1,y1,x2,y2 in queries:
+            for i in range(x1,x2+1):
+                matrix[i][y1]+=1
+                if y2+1==n:
+                    continue
+                matrix[i][y2+1]-=1
+        for i in range(n):
             prev=0
-            for i in range(n):
-                cols[i]+=prev
-                prev=cols[i]
-            for i in range(n):
-                matrix[cur][i]=cols[i]
-            cur+=1
-        return matrix
-
-        #     for i in range(x1,x2+1):
-        #         for j in range(y1,y2+1):
-        #             matrix[i][j]+=1
+            for j in range(n):
+                matrix[i][j]+=prev
+                prev=matrix[i][j]
         return matrix
