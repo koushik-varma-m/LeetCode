37e5b947a9a2cc3ea6485a6ef8c5a51a78ceb21d
Koushik Varma Mudunuri <60594906+koushik-varma-m@users.noreply.github.com>
2025-11-15 13:25:08 -0500

    Time: 1441 ms (5.19%), Space: 39.9 MB (41.13%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.py
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.py
@@ -1,19 +1,23 @@
 class Solution:
-    def rangeAddQueries(
-        self, n: int, queries: List[List[int]]
-    ) -> List[List[int]]:
-        diff = [[0] * (n + 1) for _ in range(n + 1)]
-        for row1, col1, row2, col2 in queries:
-            diff[row1][col1] += 1
-            diff[row2 + 1][col1] -= 1
-            diff[row1][col2 + 1] -= 1
-            diff[row2 + 1][col2 + 1] += 1
+    def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
+        matrix=[[0 for _ in range(n)] for _ in range(n)]
+        cur=0
+        while(cur<n):
+            cols=[0 for _ in range(n+1)]
+            for x1,y1,x2,y2 in queries:
+                if x1<=cur<=x2:
+                    cols[y1]+=1
+                    cols[y2+1]-=1
+            prev=0
+            for i in range(n):
+                cols[i]+=prev
+                prev=cols[i]
+            for i in range(n):
+                matrix[cur][i]=cols[i]
+            cur+=1
+        return matrix
 
-        mat = [[0] * n for _ in range(n)]
-        for i in range(n):
-            for j in range(n):
-                x1 = 0 if i == 0 else mat[i - 1][j]
-                x2 = 0 if j == 0 else mat[i][j - 1]
-                x3 = 0 if i == 0 or j == 0 else mat[i - 1][j - 1]
-                mat[i][j] = diff[i][j] + x1 + x2 - x3
-        return mat
+        #     for i in range(x1,x2+1):
+        #         for j in range(y1,y2+1):
+        #             matrix[i][j]+=1
+        return matrix
