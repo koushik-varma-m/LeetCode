32bcc4dd4aacd72a9d171ab911fcbb8d5dc20205
Koushik Varma Mudunuri <60594906+koushik-varma-m@users.noreply.github.com>
2025-12-06 13:59:37 -0500

    Time: 501 ms (40%), Space: 26.8 MB (34.12%) - LeetHub

--- 3578-count-partitions-with-max-min-difference-at-most-k/3578-count-partitions-with-max-min-difference-at-most-k.py
+++ 3578-count-partitions-with-max-min-difference-at-most-k/3578-count-partitions-with-max-min-difference-at-most-k.py
@@ -4,19 +4,36 @@ class Solution:
         mod = 10**9 + 7
         dp = [0] * (n + 1)
         prefix = [0] * (n + 1)
-        cnt = SortedList()
+        min_q = deque()
+        max_q = deque()
 
         dp[0] = 1
         prefix[0] = 1
-
         j = 0
+
         for i in range(n):
-            cnt.add(nums[i])
+            # maintain the maximum value queue
+            while max_q and nums[max_q[-1]] <= nums[i]:
+                max_q.pop()
+            max_q.append(i)
+
+            # maintain the minimum value queue
+            while min_q and nums[min_q[-1]] >= nums[i]:
+                min_q.pop()
+            min_q.append(i)
+
             # adjust window
-            while j <= i and cnt[-1] - cnt[0] > k:
-                cnt.remove(nums[j])
+            while max_q and min_q and nums[max_q[0]] - nums[min_q[0]] > k:
+                if max_q[0] == j:
+                    max_q.popleft()
+                if min_q[0] == j:
+                    min_q.popleft()
                 j += 1
-            dp[i + 1] = (prefix[i] - (prefix[j - 1] if j > 0 else 0)) % mod
+
+            if j > 0:
+                dp[i + 1] = (prefix[i] - prefix[j - 1] + mod) % mod
+            else:
+                dp[i + 1] = prefix[i] % mod
             prefix[i + 1] = (prefix[i] + dp[i + 1]) % mod
 
         return dp[n]
