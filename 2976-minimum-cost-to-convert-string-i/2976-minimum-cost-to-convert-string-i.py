730c7f7329d70c4dbfe72b4448c154d7343b477a
Koushik Varma Mudunuri <60594906+koushik-varma-m@users.noreply.github.com>
2026-01-29 12:11:53 -0600

    Time: 1018 ms (50.22%), Space: 21.6 MB (7.17%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.py
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.py
@@ -1,17 +1,29 @@
 class Solution:
     def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
-        adj=[[float("inf") for _ in range(26)] for _ in range(26)]
-        for x,y,z in zip(original,changed,cost):
-            adj[ord(x)-97][ord(y)-97]=min(adj[ord(x)-97][ord(y)-97],z)       
-        for k in range(26):
-            for i in range(26):
-                for j in range(26):
-                    adj[i][j]=min(adj[i][j],adj[i][k]+adj[k][j])
+        adj=[[] for _ in range(26)]
+        for x,y,z in zip(original, changed, cost):
+            adj[ord(x)-97].append((ord(y)-97,z))
+        min_costs=[]
+        for i in range(26):
+            min_costs.append(self.dfs(i,adj))
         ans=0
-        for i,j in zip(source,target):
-            if i==j:
-                continue
-            if adj[ord(i)-97][ord(j)-97]==float("inf"):
-                return -1
-            ans+=adj[ord(i)-97][ord(j)-97]
+        for s,t in zip(source,target):
+            if s!=t:
+                cur=min_costs[ord(s)-97][ord(t)-97]
+                if cur==float('inf'):
+                    return -1
+                ans+=cur
         return ans
+    def dfs(self,cur:int, adj:List[List[tuple]]) -> List[int]:
+        pq=[(0, cur)]
+        closest=[float("inf") for i in range(26)]
+        while pq:
+            cur_cost, cur_char = heapq.heappop(pq)
+            if closest[cur_char]!=float("inf"):
+                continue
+            closest[cur_char]=cur_cost
+            for tar_char, con_cost in adj[cur_char]:
+                new_cost=cur_cost+con_cost
+                if closest[tar_char]==float("inf"):
+                    heapq.heappush(pq, (new_cost,tar_char))
+        return closest
